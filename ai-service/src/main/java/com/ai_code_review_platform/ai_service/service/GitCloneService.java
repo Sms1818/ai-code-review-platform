ebda0ed4970cd0abf440dca95d91d7a8dee644f1
Sms1818 <sahilmshitole1483@gmail.com>
2026-06-12 13:15:12 +0530

    Test SSH cloning

--- ai-service/src/main/java/com/ai_code_review_platform/ai_service/service/GitCloneService.java
+++ ai-service/src/main/java/com/ai_code_review_platform/ai_service/service/GitCloneService.java
@@ -2,7 +2,6 @@ package com.ai_code_review_platform.ai_service.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -25,12 +24,6 @@ public class GitCloneService {
                     .setURI(repoUrl)
                     .setDirectory(new File(localPath))
                     .setBranch(branchName)
-                    .setCredentialsProvider(
-                            new UsernamePasswordCredentialsProvider(
-                                    "sms1818-v",
-                                    "ATATT3xFfGF0opW-DZr_ckaXh0ZcbUi8BUbrpoWK-1uJ_Pv3NGBEruOaTFhE_w1uKbq-16hDvIjpTY32MoMmQHZQ4Xvt0oNcHuch0QJ12FnS-m0iaEuU-DjnzGKbx-SmPXupTzN-YD4WMT63mCiiGy5zzMDHYy5GcurtAjbsU6FdQtVsoT1kZQk=69CE6E44"
-                            )
-                    )
                     .call();
 
             log.info(
