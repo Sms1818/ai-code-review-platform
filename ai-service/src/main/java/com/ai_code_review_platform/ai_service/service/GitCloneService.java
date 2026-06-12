18df2816a59ed851fa654fd89ef3d8ca5170b95b
Sms1818 <sahilmshitole1483@gmail.com>
2026-06-12 12:55:00 +0530

    Test repository cloning

--- ai-service/src/main/java/com/ai_code_review_platform/ai_service/service/GitCloneService.java
+++ ai-service/src/main/java/com/ai_code_review_platform/ai_service/service/GitCloneService.java
@@ -1,31 +1,49 @@
 package com.ai_code_review_platform.ai_service.service;
 
 import lombok.extern.slf4j.Slf4j;
-
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 
-import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
-
 @Service
 @Slf4j
-
 public class GitCloneService {
 
-    public void cloneRepository(String repoUrl, String branchName) {
-        try {
-            String localPath = "repositories/" + branchName;
+    public void cloneRepository(
+            String repoUrl,
+            String branchName
+    ) {
 
-            Git.cloneRepository().setURI(repoUrl).setDirectory(new File(localPath)).setBranch(branchName).call();
+        try {
+
+            String localPath =
+                    "repositories/" + branchName;
+
+            Git.cloneRepository()
+                    .setURI(repoUrl)
+                    .setDirectory(new File(localPath))
+                    .setBranch(branchName)
+                    .setCredentialsProvider(
+                            new UsernamePasswordCredentialsProvider(
+                                    "sms1818-v",
+                                    "ATATT3xFfGF0opW-DZr_ckaXh0ZcbUi8BUbrpoWK-1uJ_Pv3NGBEruOaTFhE_w1uKbq-16hDvIjpTY32MoMmQHZQ4Xvt0oNcHuch0QJ12FnS-m0iaEuU-DjnzGKbx-SmPXupTzN-YD4WMT63mCiiGy5zzMDHYy5GcurtAjbsU6FdQtVsoT1kZQk=69CE6E44"
+                            )
+                    )
+                    .call();
+
             log.info(
                     "Repository cloned successfully at {}",
-                    localPath);
+                    localPath
+            );
 
         } catch (Exception e) {
-            log.error("Error cloning repository", e);
+
+            log.error(
+                    "Error cloning repository",
+                    e
+            );
         }
     }
-
 }
