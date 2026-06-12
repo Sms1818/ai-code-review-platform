f6bb648b5a900407aea78669930a1906295064bd
Sms1818 <sahilmshitole1483@gmail.com>
2026-06-12 17:20:55 +0530

    Doing changes in main branch

--- review-service/src/main/java/com/ai_code_review_platform/review_service/controller/WebhookController.java
+++ review-service/src/main/java/com/ai_code_review_platform/review_service/controller/WebhookController.java
@@ -3,31 +3,30 @@ package com.ai_code_review_platform.review_service.controller;
 import com.ai_code_review_platform.review_service.dto.BitbucketWebhookRequest;
 import com.ai_code_review_platform.review_service.service.WebhookService;
 import lombok.RequiredArgsConstructor;
-import com.ai_code_review_platform.review_service.producer.ReviewEventProducer;
-import com.ai_code_review_platform.review_service.dto.PullRequestEventMessage;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/webhooks")
 @RequiredArgsConstructor
+@Slf4j
 public class WebhookController {
 
     private final WebhookService webhookService;
-    private final ReviewEventProducer producer;
 
     @PostMapping("/bitbucket")
-public ResponseEntity<String> handleWebhook(
-        @RequestBody BitbucketWebhookRequest request){
+    public ResponseEntity<String> handleWebhook(
+            @RequestBody BitbucketWebhookRequest request) {
 
-    webhookService.processPullRequestEvent(
-            request
-    );
+        log.info("WEBHOOK HITTTTT 🔥");
 
-    return ResponseEntity.ok(
-            "Webhook received successfully"
-    );
-}
+        webhookService.processPullRequestEvent(
+                request
+        );
 
+        return ResponseEntity.ok(
+                "Webhook received successfully"
+        );
+    }
 }
