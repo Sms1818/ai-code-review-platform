bbd70b9a2472950d511dfaf6d2043a1a3e68c2c2
Sms1818 <sahilmshitole1483@gmail.com>
2026-06-12 13:16:31 +0530

    removed credentials

--- ai-service/src/main/java/com/ai_code_review_platform/ai_service/service/GitCloneService.java
+++ ai-service/src/main/java/com/ai_code_review_platform/ai_service/service/GitCloneService.java
@@ -25,12 +25,6 @@ public class GitCloneService {
                     .setURI(repoUrl)
                     .setDirectory(new File(localPath))
                     .setBranch(branchName)
-                    .setCredentialsProvider(
-                            new UsernamePasswordCredentialsProvider(
-                                    "sms1818-v",
-                                    "ATATT3xFfGF0opW-DZr_ckaXh0ZcbUi8BUbrpoWK-1uJ_Pv3NGBEruOaTFhE_w1uKbq-16hDvIjpTY32MoMmQHZQ4Xvt0oNcHuch0QJ12FnS-m0iaEuU-DjnzGKbx-SmPXupTzN-YD4WMT63mCiiGy5zzMDHYy5GcurtAjbsU6FdQtVsoT1kZQk=69CE6E44"
-                            )
-                    )
                     .call();
 
             log.info(
