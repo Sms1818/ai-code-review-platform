1781d9d7a63cd1e29cd0a8da29664761cce11b2e
Sms1818 <sahilmshitole1483@gmail.com>
2026-06-17 20:54:07 +0530

    added try catch block for consumer

--- ai-service/src/main/java/com/ai_code_review_platform/ai_service/consumer/ReviewEventConsumer.java
+++ ai-service/src/main/java/com/ai_code_review_platform/ai_service/consumer/ReviewEventConsumer.java
@@ -1,48 +1,53 @@
 package com.ai_code_review_platform.ai_service.consumer;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.kafka.annotation.KafkaListener;
-import com.ai_code_review_platform.ai_service.service.GitCloneService;
-import com.ai_code_review_platform.ai_service.service.GitDiffService;
-import com.ai_code_review_platform.ai_service.service.AIReviewService;
 import org.springframework.stereotype.Service;
 
-import lombok.AllArgsConstructor;
 import com.ai_code_review_platform.ai_service.dto.PullRequestEventMessage;
+import com.ai_code_review_platform.ai_service.service.AIReviewService;
+import com.ai_code_review_platform.ai_service.service.GitCloneService;
+import com.ai_code_review_platform.ai_service.service.GitDiffService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 @AllArgsConstructor
 public class ReviewEventConsumer {
 
-    private final GitCloneService gitCloneService;
-    private final GitDiffService gitDiffService;
-    private final AIReviewService aiReviewService;
+        private final GitCloneService gitCloneService;
+        private final GitDiffService gitDiffService;
+        private final AIReviewService aiReviewService;
 
-    @KafkaListener(topics = "pr-review-events")
-    public void consumeEvent(PullRequestEventMessage message) {
-        log.info(
-                "Received PR Event: {}",
-                message);
+        @KafkaListener(topics = "pr-review-events")
+        public void consumeEvent(PullRequestEventMessage message) {
+                try {
+                        log.info(
+                                        "Received PR Event: {}",
+                                        message);
 
-        gitCloneService.cloneRepository(
-                message.getCloneUrl(),
-                message.getSourceBranch());
+                        gitCloneService.cloneRepository(
+                                        message.getCloneUrl(),
+                                        message.getSourceBranch());
 
-        String repoPath = "repositories/" + message.getSourceBranch();
+                        String repoPath = "repositories/" + message.getSourceBranch();
 
-        String diff = gitDiffService.generateDiff(
-                repoPath, message.getTargetBranch());
+                        String diff = gitDiffService.generateDiff(
+                                        repoPath, message.getTargetBranch());
 
-        log.info(diff);
+                        log.info(diff);
 
-        String review = aiReviewService.generateReview(
-                diff);
+                        String review = aiReviewService.generateReview(
+                                        diff);
 
-        log.info(
-                "AI REVIEW:\n{}",
-                review);
-    }
+                        log.info(
+                                        "AI REVIEW:\n{}",
+                                        review);
+                } catch (Exception e) {
+                        log.error("FAILED", e);
+                        throw e;
+                }
+        }
 
 }
